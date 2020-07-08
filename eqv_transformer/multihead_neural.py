0d4de0c97e26facf791bbe7525c42a355ed34ba1
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2020-07-08 13:33:25 +0100

    Adds new implementation of out attention

--- eqv_transformer/multihead_neural.py
+++ eqv_transformer/multihead_neural.py
@@ -9,6 +9,20 @@ from lie_conv.utils import Pass
 
 
 class MultiheadLinear(nn.Module):
+    """ Layer to perform n_heads MLPs in parallel for multihead kernels
+
+        Parameters
+        ----------
+        n_heads : int
+            number of heads
+        c_in : int
+            input dimension
+        c_out : int
+            output dimension
+        bias : bool, optional
+            use a bias parameter, by default True
+    """
+
     def __init__(self, n_heads, c_in, c_out, bias=True):
         super(MultiheadLinear, self).__init__()
 
@@ -16,9 +30,11 @@ class MultiheadLinear(nn.Module):
         self.c_in = c_in
         self.c_out = c_out
 
+        # n_head linear transforms
         self.weight = nn.Parameter(torch.Tensor(self.n_heads, self.c_in, self.c_out))
 
         if bias:
+            # n_head biases
             self.bias = nn.Parameter(torch.Tensor(self.n_heads, self.c_out))
         else:
             self.register_parameter("bias", None)
@@ -33,27 +49,28 @@ class MultiheadLinear(nn.Module):
             nn.init.uniform_(self.bias, -bound, bound)
 
     def forward(self, input):
+        # Uses einsum notation to get the correct output
         if self.bias is not None:
             return torch.einsum("...hi,hio->...ho", input, self.weight) + self.bias
         else:
             return torch.einsum("...hi,hio->...ho", input, self.weight)
 
 
-def MultiheadLinearBNact(n_heads, c_in, c_out, act="swish", bn=True):
+def MultiheadLinearBNact(c_in, c_out, n_heads, act="swish", bn=True):
     """??(from LieConv - not sure it does assume) assumes that the inputs to the net are shape (bs,n,mc_samples,c)"""
     assert act in ("relu", "swish"), f"unknown activation type {act}"
     normlayer = MaskBatchNormNd(c_out)
     return nn.Sequential(
-        Pass(MultiheadLinear(n_heads, c_in, c_out), dim=1),
+        MultiheadLinear(n_heads, c_in, c_out),
         normlayer if bn else nn.Sequential(),
-        Pass(Swish() if act == "swish" else nn.ReLU(), dim=1),
+        Swish() if act == "swish" else nn.ReLU(),
     )
 
 
-def MultiheadWeightNet(in_dim, out_dim, act, bn, hid_dim=32):
+def MultiheadWeightNet(in_dim, out_dim, n_heads, act, bn, hid_dim=32):
     return nn.Sequential(
-        *MultiheadLinearBNact(in_dim, hid_dim, act, bn),
-        *MultiheadLinearBNact(hid_dim, hid_dim, act, bn),
-        *MultiheadLinearBNact(hid_dim, out_dim, act, bn),
+        *MultiheadLinearBNact(in_dim, hid_dim, n_heads, act, bn),
+        *MultiheadLinearBNact(hid_dim, hid_dim, n_heads, act, bn),
+        *MultiheadLinearBNact(hid_dim, out_dim, n_heads, act, bn),
     )
 
