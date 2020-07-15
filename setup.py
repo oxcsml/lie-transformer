8674b1db2da38ad9732d5d57c800f629ba29aad0
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2020-07-15 10:35:10 +0100

    Added setup.py for local installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="Equivariant Attention",
+    version="0.0.1",
+    description="Regular representation based equivariant attention layers",
+    author="Michael Hutchinson, Charline Le Lan",
+    packages=["eqv_transformer"],
+)
