c6d3a0cc8d307af4124c2bb19f562331f7a92780
Sheheryar Zaidi <sheh_zaidi96@hotmail.com>
2021-06-15 16:24:05 +0100

    updated dynamics training script

--- scripts/dynamics/train_dynamics.py
+++ scripts/dynamics/train_dynamics.py
@@ -9,16 +9,12 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 torch.manual_seed(0)
 
-# torch.autograd.set_detect_anomaly(True)
-
 import forge
 from forge import flags
 import forge.experiment_tools as fet
 from copy import deepcopy
 import deepdish as dd
 from tqdm import tqdm
-from itertools import chain
-from lie_conv.hamiltonian import EuclideanK, SpringV, SpringH, HamiltonianDynamics
 from types import SimpleNamespace
 
 
@@ -29,52 +25,11 @@ from eqv_transformer.train_tools import (
     print_reports,
     load_checkpoint,
     save_checkpoint,
-    ExponentialMovingAverage,
     nested_to,
     param_count,
-    get_component,
-    get_average_norm,
-    # plot_grad_flow
 )
+from attrdict import AttrDict
 
-# %%
-# from matplotlib.lines import Line2D
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# def plot_grad_flow(named_parameters, save_dir):
-#     '''Plots the gradients flowing through different layers in the net during training.
-#     Can be used for checking for possible gradient vanishing / exploding problems.
-
-#     Usage: Plug this function in Trainer class after loss.backwards() as
-#     "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
-#     start_time = time.time()
-#     with torch.no_grad():
-#         ave_grads = []
-#         max_grads= []
-#         layers = []
-#         for n, p in named_parameters:
-#             if(p.requires_grad) and ("bias" not in n):
-#                 layers.append(n)
-#                 ave_grads.append(p.grad.abs().mean())
-#                 max_grads.append(p.grad.abs().max())
-#         plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
-#         plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
-#         plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k" )
-#         plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
-#         plt.xlim(left=0, right=len(ave_grads))
-#         plt.ylim(bottom = -0.001, top=0.02) # zoom in on the lower gradient regions
-#         plt.xlabel("Layers")
-#         plt.ylabel("average gradient")
-#         plt.title("Gradient flow")
-#         plt.grid(True)
-#         plt.legend([Line2D([0], [0], color="c", lw=4),
-#                     Line2D([0], [0], color="b", lw=4),
-#                     Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
-
-#         plt.savefig(save_dir)
-
-#     print(f"Gradient flow plot took {time.time() - start_time:.2f} sec.")
 
 # %%
 
@@ -111,17 +66,11 @@ flags.DEFINE_integer(
     "report_loss_every", 10, "Number of iterations between reporting minibatch loss."
 )
 flags.DEFINE_integer(
-    "evaluate_every", 10000, "Number of iterations between reporting validation loss."
-)
-flags.DEFINE_integer(
     "save_check_points",
     50,
     "frequency with which to save checkpoints, in number of epoches.",
 )
 flags.DEFINE_boolean("log_train_values", True, "Logs train values if True.")
-# flags.DEFINE_float(
-#     "ema_alpha", 0.99, "Alpha coefficient for exponential moving average of train logs."
-# )
 
 # Optimization
 flags.DEFINE_integer("train_epochs", 200, "Maximum number of training epochs.")
@@ -129,15 +78,7 @@ flags.DEFINE_integer("batch_size", 100, "Mini-batch size.")
 flags.DEFINE_float("learning_rate", 1e-3, "Adam learning rate.")
 flags.DEFINE_float("beta1", 0.9, "Adam Beta 1 parameter")
 flags.DEFINE_float("beta2", 0.999, "Adam Beta 2 parameter")
-flags.DEFINE_string(
-    "lr_schedule", "cosine_annealing", "Learning rate schedule."
-)  # TODO: need to match LieConv one. currently using PyTorch one, is it the same?
-flags.DEFINE_boolean(
-    "clip_grad_norm", False, "Clip norm of the gradient at max_grad_norm."
-)
-flags.DEFINE_integer(
-    "max_grad_norm", 100, "Maximum norm of gradient when clip_grad_norm is True."
-)
+flags.DEFINE_string("lr_schedule", "cosine_annealing", "Learning rate schedule.")
 
 # GPU device
 flags.DEFINE_integer("device", 0, "GPU to use.")
@@ -145,14 +86,13 @@ flags.DEFINE_integer("device", 0, "GPU to use.")
 # Debug mode tracks more stuff
 flags.DEFINE_boolean("debug", True, "Track and show on tensorboard more metrics.")
 flags.DEFINE_boolean(
-    "kill_if_poor", False, "Kills run if loss is poor. Exact params to be set below."
-)
-flags.DEFINE_boolean(
     "save_test_predictions",
     True,
     "Makes and saves test predictions on one or more test sets (e.g. 5-step and 100-step predictions) at the end of training.",
 )
-
+flags.DEFINE_boolean(
+    "log_val_test", True, "Turns off computation of validation and test errors."
+)
 #####################################################################################################################
 
 
@@ -206,7 +146,6 @@ def main():
         ("n_train", "ntrain"),
         ("group", "group"),
         ("lift_samples", "ls"),
-        # ("")
     ]
 
     run_name = ""  # config.run_name
@@ -218,9 +157,6 @@ def main():
             run_name += abbrev
             run_name += str(getattr(config, attr))
             run_name += "_"
-
-    if config.clip_grad_norm:
-        run_name += "clipnorm" + str(config.max_grad_norm) + "_"
 
     results_folder_name = osp.join(
         data_name,
@@ -247,23 +183,12 @@ def main():
         model_params, lr=opt_learning_rate, betas=(config.beta1, config.beta2)
     )
 
-    # model_opt = torch.optim.SGD(
-    #     model_params, lr=opt_learning_rate#, betas=(config.beta1, config.beta2)
-    # )
-
     if config.lr_schedule == "cosine_annealing":
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
             model_opt, config.train_epochs
         )
-    elif config.lr_schedule == "cosine_annealing_warmup":
-        num_warmup_epochs = int(0.05 * config.train_epochs)
-        num_warmup_steps = len(train_loader) * num_warmup_epochs
-        num_training_steps = len(train_loader) * config.train_epochs
-        scheduler = transformers.get_cosine_schedule_with_warmup(
-            model_opt,
-            num_warmup_steps=num_warmup_steps,
-            num_training_steps=num_training_steps,
-        )
+    else:
+        raise NotImplementedError(f"{config.lr_schedule} not implemented.")
 
     # Try to restore model and optimizer from checkpoint
     if resume_checkpoint is not None:
@@ -283,37 +208,11 @@ def main():
     train_reports = []
     report_all = {}
     report_all_val = {}
+
     # Saving model at epoch 0 before training
     print("saving model at epoch 0 before training ... ")
     save_checkpoint(checkpoint_name, 0, model, model_opt, loss=0.0)
     print("finished saving model at epoch 0 before training")
-
-    if (
-        config.debug
-        and config.model_config == "configs/dynamics/eqv_transformer_model.py"
-    ):
-        model_components = (
-            [(0, [], "embedding_layer")]
-            + list(
-                chain.from_iterable(
-                    (
-                        (k, [], f"ema_{k}"),
-                        (
-                            k,
-                            ["ema", "kernel", "location_kernel"],
-                            f"ema_{k}_location_kernel",
-                        ),
-                        (
-                            k,
-                            ["ema", "kernel", "feature_kernel"],
-                            f"ema_{k}_feature_kernel",
-                        ),
-                    )
-                    for k in range(1, config.num_layers + 1)
-                )
-            )
-            + [(config.num_layers + 2, [], "output_mlp")]
-        )  # components to track for debugging
 
     num_params = param_count(model)
     print(f"Number of model parameters: {num_params}")
@@ -322,15 +221,17 @@ def main():
     start_t = time.time()
 
     total_train_iters = len(train_loader) * config.train_epochs
-    iters_per_eval = int(total_train_iters / 100) # evaluate 100 times over the course of training
+    iters_per_eval = int(
+        total_train_iters / 100
+    )  # evaluate 100 times over the course of training
 
     assert (
         config.n_train % min(config.batch_size, config.n_train) == 0
-    ), "Batch size doesn't divide dataset size. Can be problematic for loss computation (see below)."
+    ), "Batch size doesn't divide dataset size. Can be inaccurate for loss computation (see below)."
 
     grad_flows = []
     training_failed = False
-    best_val_loss_so_far = 1e+7
+    best_val_loss_so_far = 1e7
 
     for epoch in tqdm(range(start_epoch, config.train_epochs + 1)):
         model.train()
@@ -339,7 +240,14 @@ def main():
             data = nested_to(
                 data, device, torch.float32
             )  # the format is ((z0, sys_params, ts), true_zs) for data
-            outputs = model(data)
+            true_zs = data[-1]
+            if config.model_with_dict:
+                outputs = model(data)
+            else:
+                pred_zs = model(data)
+                loss = (pred_zs - true_zs).pow(2).mean()
+                outputs = AttrDict({"loss": loss, "prediction": pred_zs})
+                outputs.reports = AttrDict({"mse": loss})
 
             if torch.isnan(outputs.loss):
                 if not training_failed:
@@ -350,14 +258,6 @@ def main():
 
             model_opt.zero_grad()
             outputs.loss.backward(retain_graph=False)
-
-            # if config.debug and (batch_idx % config.report_loss_every == 0):
-            #     plot_grad_flow(model.named_parameters(), osp.join(logdir, "grad_flow.pdf"))
-
-            if config.clip_grad_norm:
-                torch.nn.utils.clip_grad_norm_(
-                    model.parameters(), config.max_grad_norm, norm_type=1
-                )
 
             model_opt.step()
 
@@ -386,102 +286,10 @@ def main():
             if config.lr_schedule == "cosine_annealing_warmup":
                 scheduler.step()
 
-            # Track stuff for debugging
-            if config.debug:
-                model.eval()
-                with torch.no_grad():
-                    curr_params = {
-                        k: v.detach().clone() for k, v in model.state_dict().items()
-                    }
-
-                    # updates norm
-                    if train_iter == 1:
-                        update_norm = 0
-                    else:
-                        update_norms = []
-                        for (k_prev, v_prev), (k_curr, v_curr) in zip(
-                            prev_params.items(), curr_params.items()
-                        ):
-                            assert k_prev == k_curr
-                            if (
-                                "tracked" not in k_prev
-                            ):  # ignore batch norm tracking. TODO: should this be ignored? if not, fix!
-                                update_norms.append((v_curr - v_prev).norm(1).item())
-                        update_norm = sum(update_norms)
-
-                    # gradient norm
-                    grad_norm = 0
-                    for p in model.parameters():
-                        try:
-                            grad_norm += p.grad.norm(1)
-                        except AttributeError:
-                            pass
-
-                    # weights norm
-                    # if config.model_config == 'configs/dynamics/eqv_transformer_model.py':
-                    #     model_norms = {}
-                    #     for comp_name in model_components:
-                    #         comp = get_component(model.predictor.net, comp_name)
-                    #         norm = get_average_norm(comp)
-                    #         model_norms[comp_name[2]] = norm
-
-                    #     log_tensorboard(
-                    #         summary_writer,
-                    #         train_iter,
-                    #         model_norms,
-                    #         "debug/avg_model_norms/",
-                    #     )
-
-                    # Average V size:
-                    (z0, sys_params, ts), true_zs = data
-
-                    z = z0
-                    m = sys_params[..., 0]  # assume the first component encodes masses
-                    D = z.shape[-1]  # of ODE dims, 2*num_particles*space_dim
-                    q = z[:, : D // 2].reshape(*m.shape, -1)
-                    k = sys_params[..., 1]
-
-                    V = model.predictor.compute_V((q, sys_params))
-                    V_true = SpringV(q, k)
-
-                    log_tensorboard(
-                        summary_writer,
-                        train_iter,
-                        {
-                            "avg_update_norm1": update_norm / num_params,
-                            "avg_grad_norm1": grad_norm / num_params,
-                            "avg_predicted_potential_norm": V.norm(1) / V.numel(),
-                            "avg_true_potential_norm": V_true.norm(1) / V_true.numel(),
-                        },
-                        "debug/",
-                    )
-                    prev_params = curr_params
-
-                    # gradient flow
-                    ave_grads = []
-                    max_grads = []
-                    layers = []
-                    for n, p in model.named_parameters():
-                        if (p.requires_grad) and (
-                            p.grad is not None
-                        ):  # and ("bias" not in n): # SZ: let's track bias grads too
-                            layers.append(n)
-                            ave_grads.append(p.grad.abs().mean().item())
-                            max_grads.append(p.grad.abs().max().item())
-
-                    grad_flow = {
-                        "layers": layers,
-                        "ave_grads": ave_grads,
-                        "max_grads": max_grads,
-                    }
-                    grad_flows.append(grad_flow)
-
-                model.train()
-
             # Logging
-            if train_iter % iters_per_eval == 0 or (
-                train_iter == total_train_iters
-            ):  # batch_idx % config.evaluate_every == 0:
+            if (
+                train_iter % iters_per_eval == 0 or (train_iter == total_train_iters)
+            ) and config.log_val_test:  # batch_idx % config.evaluate_every == 0:
                 model.eval()
                 with torch.no_grad():
 
@@ -502,7 +310,7 @@ def main():
                     for k, v in reports.items():
                         reports[k] = v / len(
                             test_loader
-                        )  # SZ: note this is slightly incorrect since mini-batch sizes can vary (if batch_size doesn't divide train_size), but approximately correct.
+                        )  # SZ: note this can be slightly incorrect if mini-batch sizes vary (if batch_size doesn't divide train_size), but approximately correct.
 
                     reports = parse_reports(reports)
                     reports["time"] = time.time() - start_t
@@ -525,13 +333,6 @@ def main():
                         prefix="test",
                     )
 
-                    if config.kill_if_poor:
-                        if epoch > config.train_epochs * 0.2:
-                            if reports["mse"] > 0.01:
-                                raise RuntimeError(
-                                    f"Killed run due to poor performance."
-                                )
-
                     # repeat for validation data
                     reports = None
                     for data in val_loader:
@@ -548,9 +349,7 @@ def main():
                                 reports[k] += v.detach().clone().cpu()
 
                     for k, v in reports.items():
-                        reports[k] = v / len(
-                            val_loader
-                        )  # SZ: note this is slightly incorrect since mini-batch sizes can vary (if batch_size doesn't divide train_size), but approximately correct.
+                        reports[k] = v / len(val_loader)
 
                     reports = parse_reports(reports)
                     reports["time"] = time.time() - start_t
@@ -573,9 +372,15 @@ def main():
                         prefix="val",
                     )
 
-                    if report_all_val['mse'][-1] < best_val_loss_so_far:
-                        save_checkpoint(checkpoint_name, f"early_stop", model, model_opt, loss=outputs.loss)
-                        best_val_loss_so_far = report_all_val['mse'][-1] 
+                    if report_all_val["mse"][-1] < best_val_loss_so_far:
+                        save_checkpoint(
+                            checkpoint_name,
+                            f"early_stop",
+                            model,
+                            model_opt,
+                            loss=outputs.loss,
+                        )
+                        best_val_loss_so_far = report_all_val["mse"][-1]
 
                 model.train()
 
@@ -605,9 +410,11 @@ def main():
         for chunk_len in [5, 100]:
             start_t_preds = time.time()
             data_config = SimpleNamespace(
-                **{**config.__dict__["__flags"], **{"chunk_len": chunk_len, "batch_size": 500}}
+                **{
+                    **config.__dict__["__flags"],
+                    **{"chunk_len": chunk_len, "batch_size": 500},
+                }
             )
-            # print(data_config)
             dataloaders, data_name = fet.load(config.data_config, config=data_config)
             test_loader_preds = dataloaders["test"]
 
@@ -619,11 +426,9 @@ def main():
                 for idx, d in enumerate(test_loader_preds):
                     true.append(d[-1])
                     d = nested_to(d, device, torch.float32)
-                    # print(d[-1].shape)
                     outputs = model(d)
 
                     pred_zs = outputs.prediction
-                    # (z0, sys_params, ts), true_zs = d
                     preds.append(pred_zs)
 
                     num_datapoints += len(pred_zs)
@@ -634,14 +439,10 @@ def main():
                 preds = torch.cat(preds, dim=0).cpu()
                 true = torch.cat(true, dim=0).cpu()
 
-                save_dir = osp.join(
-                    logdir, f"traj_preds_{chunk_len}_steps_2k_test.pt"
-                )
+                save_dir = osp.join(logdir, f"traj_preds_{chunk_len}_steps_2k_test.pt")
                 torch.save(preds, save_dir)
 
-                save_dir = osp.join(
-                    logdir, f"traj_true_{chunk_len}_steps_2k_test.pt"
-                )
+                save_dir = osp.join(logdir, f"traj_true_{chunk_len}_steps_2k_test.pt")
                 torch.save(true, save_dir)
 
                 print(
